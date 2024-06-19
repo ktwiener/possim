4b389ea61cb5c5f016d256ee649241d312621fe3
Catie Wiener <catiewien@gmail.com>
2024-06-19 16:39:43 -0500

    results for ser

---
+++ scripts/results_ser.R
@@ -0,0 +1,76 @@
+## Results
+library(gt)
+library(ggplot2)
+library(dplyr)
+all_scripts <- list.files("R", pattern = "*.R", full.names = T)
+purrr::walk(all_scripts, source)
+date <- "2024-05-02"
+
+measures <- readRDS(sprintf("data/results/raw/%s-measures.rds", date))
+
+mcse <- measures |>
+  dplyr::filter(grepl("lnrr", Param)) %>%
+  dplyr::group_by(positivity, efftype, probw, Param) |>
+  dplyr::mutate(expest = mean(Coef), nsim = n()) |>
+  dplyr::summarize(
+    mcvar_bias = (1/(nsim[1]*(nsim[1]-1)))*sum((Coef-expest)^2)
+  ) |>
+  dplyr::mutate(
+    mcse_bias = sqrt(mcvar_bias)
+  ) %>%
+  pull(mcse_bias)
+
+print(paste0("Monte-Carlo SE: ", min(mcse), max(mcse)))
+## Table
+gttbl <- make_table(measures)
+
+gttbl |>
+  gt::gtsave(paste0(date, "-rr.rtf"), "data/results/tables/")
+
+## Table
+gttblor <- make_table(measures, "lnor")
+
+gttblor |>
+  gt::gtsave(paste0(date, "-or.rtf"), "data/results/tables/")
+## Box Plots
+
+measures %>%
+  dplyr::filter(grepl("lnrr", Param)) %>%
+  dplyr::filter(positivity == "Full") %>%
+boxplot() +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+
+
+ ggsave(filename = sprintf("data/results/figures/%s-homogeneous-hajek-box-complete.png", date),
+       units = "in",
+       width = 6.5,
+       height = 5.6
+)
+ measures %>%
+   dplyr::filter(grepl("lnrr", Param)) %>%
+   dplyr::filter(positivity == "Partial") %>%
+   boxplot()+
+   theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+
+
+ ggsave(filename = sprintf("data/results/figures/%s-homogeneous-hajek-box-partial.png", date),
+        units = "in",
+        width = 6.5,
+        height = 5.6
+ )
+
+boxplot(measures, effect = "None")
+
+ggsave(filename = sprintf("data/results/figures/%s-none-hajek-box.png", date),
+       units = "in",
+       width = 6,
+       height = 6
+)
+
+boxplot(measures, ipttype = "ht")
+
+ggsave(filename = sprintf("data/results/figures/%s-homogeneous-ht-box.png", date),
+       units = "in",
+       width = 6,
+       height = 6
+)
